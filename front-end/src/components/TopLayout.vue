efd185494c1720d192c2631b236e0bcdeb49fbce
zkzkzhzj <zkzkzhzj@gmail.com>
2020-12-26 14:10:43 +0900

    add TopLayout End

--- front-end/src/components/TopLayout.vue
+++ front-end/src/components/TopLayout.vue
@@ -1,17 +1,17 @@
 <template>
-	<div id="main">
-		<div>
-			<button type="button" id="burger-button">
+	<div id="main-top">
+		<div id="main-top-div-burger">
+			<button type="button" id="main-top-burger-button">
 				<span></span>
 				<span></span>
 				<span></span>
 			</button>
 		</div>
-		<div>
-			<img id="main-logo" src="../assets/logo.png" />
+		<div id="main-top-div-logo">
+			<img id="main-top-logo" src="../assets/logo.png" />
 		</div>
-		<div>
-			<span>로그인ㅣ회원가입</span>
+		<div id="main-top-div-login">
+			<span id="main-top-login">로그인 | 회원가입</span>
 		</div>
 	</div>
 </template>
@@ -25,17 +25,20 @@
 	$main-color: rgb(26, 83, 92);
 	$main-color-hover: rgba(26, 83, 92, 0.5);
 
-	div#main {
-		width: 100%;
+	div#main-top {
+		width: 99%;
+		height: $top-main-size;
 		display: flex;
 		position: fixed;
 		justify-content: space-between;
+		align-items: center;
 		div {
 			width: 100%;
-			#burger-button {
-				position: absolute;
+		}
+		#main-top-div-burger {
+			#main-top-burger-button {
 				width: $top-main-size;
-				background-color: #fff;
+				background-color: rgba(0, 0, 0, 0);
 				border: 0px;
 				span {
 					display: block;
@@ -46,13 +49,39 @@
 					border-radius: 10px;
 				}
 			}
-			#burger-button:hover {
+			#main-top-burger-button:hover {
+				cursor: pointer;
 				span {
 					background-color: $main-color-hover;
 				}
 			}
-			#main-logo {
+			#main-top-burher-button:focus {
+				outline: none;
+				box-shadow: none;
+			}
+		}
+		#main-top-div-logo {
+			text-align: center;
+			#main-top-logo {
 				height: $top-main-size;
+			}
+			#main-top-logo {
+				cursor: pointer;
+			}
+		}
+		#main-top-div-login {
+			text-align: end;
+			#main-top-login {
+				font-size: 12px;
+				font-weight: bold;
+				color: $main-color;
+				margin-right: 5px;
+				padding: 10px;
+			}
+			#main-top-login:hover {
+				cursor: pointer;
+				opacity: 0.5;
+				filter: alpha(opacity=50);
 			}
 		}
 	}
