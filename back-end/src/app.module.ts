2dc8d53ba7b191b4be8fd20a9f6278c264cdb06e
imgomi <webmaster@mail.gomi.land>
2020-12-31 19:47:54 +0900

    modify : Modify project settings

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,13 +3,49 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { ArticlesModule } from './articles/articles.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ServersModule } from './servers/servers.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, ArticlesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev' ? 'env/.dev.env' : 'env/.test.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UsersModule,
+    ArticlesModule,
+    ServersModule,
+  ],
   controllers: [],
   providers: [],
 })
 export class AppModule {
-  constructor(private connection: Connection) {
-  }
+  constructor(private connection: Connection) {}
 }
